965a23f09bc3b198f684264f8d5142e223d028e2
tribal-developer <laxmanmarskole2002@gmail.com>
2026-05-18 09:54:46 -0700

    deployment ready setup

--- cyber_portfolio/cyber_portfolio/settings.py
+++ cyber_portfolio/cyber_portfolio/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-sesl4^6udzs&a!^c@=lbosw_#2vik8jp%4%39q-+c@^sx-hif9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.onrender.com',
+    'localhost',
+    '127.0.0.1'
+]
 
 
 # Application definition
@@ -42,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -116,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media Files
 MEDIA_URL = '/media/'
